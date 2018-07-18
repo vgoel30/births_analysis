51828e1a090f9c8f3190bc28e3805da4596cf768
vgoel30 <varun.goel@stonybrook.edu>
2018-07-18 19:34:23 +0530

    Added heatmap

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 import os
 from math import log as ln
 
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
 def get_year_distribution(datafile, year):
 	year = str(year)
 	with open(datafile) as f:
@@ -106,6 +110,45 @@ def get_combined_day_births(datafile):
 						births[int(month)][int(date)] = 0
 					births[int(month)][int(date)] += month_data[date]['births']
 	pprint(births)
+
+	months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+	date_list = [x for x in range(1, 32)]
+
+	z = []
+	i = 0
+
+	for month in births:
+		i += 1
+		month_data = births[i]
+		new_row = []
+		for date in date_list:
+			if date in month_data:
+				new_row.append(month_data[date])
+			else:
+				new_row.append(0)
+		z.append(new_row)
+
+	z[1][28] *= 4
+
+	#pprint(z)
+
+	data = [
+    go.Heatmap(
+        z=z,
+        x=date_list,
+        y=months,
+        colorscale='Jet',
+    	)
+	]
+
+	layout = go.Layout(
+	    title='Births per date from 1994-2014',
+	    xaxis = dict(ticks='', nticks=31),
+	    yaxis = dict(ticks='' )
+	)
+
+	fig = go.Figure(data=data, layout=layout)
+	plotly.offline.plot(fig, filename='datetime-heatmap')
 
 	# births = {month: births[month]/100000 for month in births}
 	# lists = births.items() # sorted by key, return a list of tuples
